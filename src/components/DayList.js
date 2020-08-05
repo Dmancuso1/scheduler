47c0b0db47e2935e312e455a10cbfb1a05540d67
Dmancuso1 <danegmancuso@gmail.com>
2020-08-04 22:05:16 -0400

    added appointment (with form) components to story book and tested

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,13 +1,33 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
+
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
+
+
 export default function DayList(props) {
   console.log(props);
   const days = props.days;
   const dayListItems = days.map(day => {
     return (
       <DayListItem
-      id = {day.id}
+      key = {day.id}
       name={day.name}
       spots={day.spots}
       selected={day.name === props.day}
